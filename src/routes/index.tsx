d3798374d1f8900d2a08d399ecd73737b9ea6f05
Dhamivibez <0xdhamivibez@gmail.com>
2025-05-03 18:16:19 +0100

    feat: add Footer component to enhance site structure

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,4 @@
+import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 import About from '@/components/sections/About';
 import Contact from '@/components/sections/Contact';
@@ -19,6 +20,7 @@ function App() {
       <About />
       <Projects />
       <Contact />
+      <Footer />
     </div>
   );
 }
